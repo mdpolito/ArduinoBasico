e0da45e43ed570ffa0c02e47ee6402a496d64124
mdpolito <42000159+mdpolito@users.noreply.github.com>
2018-08-10 22:06:43 -0300

    Create LeituraAnalogica.ino

---
+++ 02 - Leitura Analogica/LeituraAnalogica.ino
@@ -0,0 +1,22 @@
+/*
+ * Lê uma porta analógica e informa o estado pela serial
+ */
+
+int pinoEntrada = A1;
+int leituraPino;
+
+void setup() {  
+  //Define a velocidade da transmissão serial
+  Serial.begin(9600);
+}
+
+void loop() { 
+  //Lê o pino de entrada
+  leituraPino = analogRead(pinoEntrada);
+
+  //Informa o resultado da leitura pela porta serial
+  Serial.println(leituraPino);
+
+  //Delay de 0,5s
+  delay(500);
+}
