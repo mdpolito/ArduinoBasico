c034d42d9e238192b34de3f520b5de232677cc3c
mdpolito <42000159+mdpolito@users.noreply.github.com>
2018-09-22 11:06:21 -0300

    Update BracoRobotico.ino

--- 07 - Braco Robotico/BracoRobotico.ino
+++ 07 - Braco Robotico/BracoRobotico.ino
@@ -1,17 +1,17 @@
 #include <Servo.h>
 
-int controleServoCabeca = A0;
+int controleServoGarra = A0;
 int controleServoProf = A1;
 int controleServoAlt = A2;
 int controleServoRot = A3;
 
-int pinoServoCabeca = 11;
+int pinoServoGarra = 11;
 int pinoServoProf = 9;
 int pinoServoAlt = 5;
 int pinoServoRot = 3;
 
-int anguloMinCabeca = 0;
-int anguloMaxCabeca = 35;
+int anguloMinGarra = 0;
+int anguloMaxGarra = 35;
 
 int anguloMinProf = 45;
 int anguloMaxProf = 180;
@@ -22,20 +22,20 @@ int anguloMaxAlt = 180;
 int anguloMinRot = 0;
 int anguloMaxRot = 180;
 
-Servo servoCabeca;
+Servo servoGarra;
 Servo servoProf;
 Servo servoAlt;
 Servo servoRot;
 
 void setup() {
-  servoCabeca.attach(pinoServoCabeca);
+  servoGarra.attach(pinoServoGarra);
   servoProf.attach(pinoServoProf);
   servoAlt.attach(pinoServoAlt);
   servoRot.attach(pinoServoRot);
 }
 
 void loop() {
-  servoCabeca.write(map(analogRead(controleServoCabeca),0,1023,anguloMinCabeca,anguloMaxCabeca));
+  servoGarra.write(map(analogRead(controleServoGarra),0,1023,anguloMinGarra,anguloMaxGarra));
   servoProf.write(map(analogRead(controleServoProf),0,1023,anguloMinProf,anguloMaxProf));
   servoAlt.write(map(analogRead(controleServoAlt),0,1023,anguloMinAlt,anguloMaxAlt));
   servoRot.write(map(analogRead(controleServoRot),0,1023,anguloMinRot,anguloMaxRot));
